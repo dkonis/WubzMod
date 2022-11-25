6d740b1917c76ebd2b416cc60fde3c7c4c6f33ec
dkonis <dkonis1@outlook.com>
2022-11-25 02:51:44 +0200

    Using Inject instead of Overwrite

--- src/main/java/net/wubz/wubzmod/mixin/WorldBorderInitializeS2CPacketMixin.java
+++ src/main/java/net/wubz/wubzmod/mixin/WorldBorderInitializeS2CPacketMixin.java
@@ -1,11 +1,12 @@
 package net.wubz.wubzmod.mixin;
 
 import net.minecraft.network.listener.ClientPlayPacketListener;
-import net.minecraft.network.packet.s2c.play.GameStateChangeS2CPacket;
 import net.minecraft.network.packet.s2c.play.WorldBorderInitializeS2CPacket;
 import net.wubz.wubzmod.WubzMod;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldBorderInitializeS2CPacket.class)
 public class WorldBorderInitializeS2CPacketMixin {
@@ -16,11 +17,9 @@ public class WorldBorderInitializeS2CPacketMixin {
      * Bypassing LiveOverflow's border by not applying
      * the WorldBorderInitialize Packet
      */
-    @Overwrite
-    public void apply(ClientPlayPacketListener clientPlayPacketListener) {
-        WorldBorderInitializeS2CPacket packet = (WorldBorderInitializeS2CPacket) (Object) this;
-        if(!WubzMod.LiveOverflowBypass){
-            clientPlayPacketListener.onWorldBorderInitialize(packet);
-        }
+    @Inject(at = @At("HEAD") , method = "apply", cancellable = true)
+    public void apply(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo ci) {
+        if(WubzMod.LiveOverflowBypass)
+            ci.cancel();
     }
 }
